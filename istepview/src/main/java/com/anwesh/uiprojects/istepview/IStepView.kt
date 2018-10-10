9087044294c673b7e8a7525fff4926ce29988f14
Anwesh43 <anweshthecool0@gmail.com>
2018-10-10 09:36:08 +0530

    adding IStepView class

--- istepview/src/main/java/com/anwesh/uiprojects/istepview/IStepView.kt
+++ istepview/src/main/java/com/anwesh/uiprojects/istepview/IStepView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.istepview
 /**
  * Created by anweshmishra on 10/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+
+val nodes : Int = 5
