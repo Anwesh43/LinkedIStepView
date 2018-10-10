60eaf6975aa1a9f5edba851d8d73c8cf03dcc4c0
Anwesh43 <anweshthecool0@gmail.com>
2018-10-10 09:59:47 +0530

    adding ISNode clas

--- istepview/src/main/java/com/anwesh/uiprojects/istepview/IStepView.kt
+++ istepview/src/main/java/com/anwesh/uiprojects/istepview/IStepView.kt
@@ -99,4 +99,47 @@ class IStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ISNode(var i : Int, val state : State = State()) {
+        private var next : ISNode? = null
+        private var prev : ISNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ISNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawISNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ISNode {
+            var curr : ISNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
